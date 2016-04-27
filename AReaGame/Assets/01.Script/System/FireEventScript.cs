a204836d515a6594dcbd25991156d8843e7a70b0
EuihyunOh <poweroeh@seoultech.ac.kr>
2016-04-28 00:26:33 +0900

    터치기능 완성
    터치기능은 완성됨. 충돌시 이벤트 추가 필요

--- AReaGame/Assets/01.Script/System/FireEventScript.cs
+++ AReaGame/Assets/01.Script/System/FireEventScript.cs
@@ -13,10 +13,10 @@ public class FireEventScript : MonoBehaviour {
     int shootableMask;//맞는 유닛인지 확인
     LineRenderer gunLine;//총알 효과
     AudioSource gunAudio;//총소리
-    float effectsDisplayTime = 0.2f;
+    float effectsDisplayTime = 0.15f;
     Vector3 shootTarget;//쏠 위치
     Vector3 shootDir;//쏘는 방향
-    
+    //나중에 실행됐다는 상태 변수도 추가해야함
     void Awake()
     {
         shootableMask = LayerMask.GetMask("Shootable");
@@ -32,16 +32,16 @@ public class FireEventScript : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-     if((Input.GetTouch(0).phase == TouchPhase.Began) && timer >= timeBetweenBullets && singleTab)
-        {
-            Shoot();
-        }
-        if(timer >= timeBetweenBullets * effectsDisplayTime)
-        {
-            DisableEffects();
-            singleTab = true;
-        }
+            timer += Time.deltaTime;
+            if (Input.GetMouseButtonDown(0) && timer >= timeBetweenBullets && singleTab)
+            {
+                Shoot();
+            }
+            if (timer >= timeBetweenBullets * effectsDisplayTime)
+            {
+                DisableEffects();
+                singleTab = true;
+            }
     }
 
     public void DisableEffects()
@@ -61,19 +61,16 @@ public class FireEventScript : MonoBehaviour {
 
         GameObject cm = VuforiaManager.Instance.ARCameraTransform.gameObject;
         Camera[] cam = cm.GetComponentsInChildren<Camera>();
-        touchRay = cam[0].ScreenPointToRay(Input.mousePosition);
-        
-
-        touchRay.origin = transform.position;
+        touchRay = cam[0].ScreenPointToRay(Input.GetTouch(0).position);
 
 
         if (Physics.Raycast(touchRay, out touchHit))
         {
+            Debug.Log("Hit : " + touchHit.collider.gameObject.name);
             gunLine.SetPosition(1, touchHit.point);
             //takedamage ---> 맞았을때 효과를 추가한다.
         }
-        else
-            gunAudio.Play();
+        
     }
 
 }
