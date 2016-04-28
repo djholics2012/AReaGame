2775c32c2564caa44b49b35edd483497d6252f7a
EuihyunOh <poweroeh@seoultech.ac.kr>
2016-04-28 16:05:52 +0900

    레이저가 물체 통과 못하게 수정
    물체에서 발사하는 래이캐스트 추가

--- AReaGame/Assets/01.Script/System/FireEventScript.cs
+++ AReaGame/Assets/01.Script/System/FireEventScript.cs
@@ -8,8 +8,10 @@ public class FireEventScript : MonoBehaviour {
     bool singleTab;
     float timer;
     float speed;
-    RaycastHit touchHit;
-    Ray touchRay;
+    RaycastHit touchHit;// 터치해서 맞은곳
+    RaycastHit shootHit;//실제로 쏴서 맞은곳
+    Ray touchRay;//터치 레이캐스트
+    Ray shootRay;//발사 레이캐스트
     int shootableMask;//맞는 유닛인지 확인
     LineRenderer gunLine;//총알 효과
     AudioSource gunAudio;//총소리
@@ -63,11 +65,16 @@ public class FireEventScript : MonoBehaviour {
         Camera[] cam = cm.GetComponentsInChildren<Camera>();
         touchRay = cam[0].ScreenPointToRay(Input.GetTouch(0).position);
 
+        Physics.Raycast(touchRay, out touchHit);//터치한 지점 탐색
 
-        if (Physics.Raycast(touchRay, out touchHit))
+        shootRay.origin = transform.position;
+        shootRay.direction = touchHit.point - transform.position;
+
+
+        if (Physics.Raycast(shootRay, out shootHit))
         {
-            Debug.Log("Hit : " + touchHit.collider.gameObject.name);
-            gunLine.SetPosition(1, touchHit.point);
+            Debug.Log("Hit : " + shootHit.collider.gameObject.name);
+            gunLine.SetPosition(1, shootHit.point);
             //takedamage ---> 맞았을때 효과를 추가한다.
         }
         
